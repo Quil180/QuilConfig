bdbbba56ec920498bd2e93e901e8477dc2203ec6
Quil180 <quil180gaming@gmail.com>
2023-12-03 16:57:01 -0500

    Got rid of the redundant waybar .config file.
    It was not in use and therefore is unneeded.

--- .config/waybar/to_mocha.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-killall waybar
-SDIR="$HOME/.config/waybar"
-waybar -c "$SDIR"/mocha.jsonc -s "$SDIR"/mocha.css &
-killall swaybg
-swaybg -i ~/Downloads/mocha-noise4-wallhaven-5gwmd3_3840x2160.png
-
