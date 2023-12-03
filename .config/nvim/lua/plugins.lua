60666d5fe66a10933bdfad44f2601a34f4a05642
Quil180 <quil180gaming@gmail.com>
2023-12-03 16:55:20 -0500

    Added the config files for nvim as well as ranger.

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,6 @@
+return require('packer').startup(function(use)
+  -- Configurations will go here soon
+ use 'wbthomason/packer.nvim'
+ use 'williamboman/mason.nvim'
+ use 'williamboman/mason-lspconfig.nvim'
+end)
